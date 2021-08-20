5b968e4ffadd16e774c1295cf13e6f7b9fc6338d
syndepzaivippro <synvippro@hotmail.com>
2021-08-20 17:02:44 +0700

    first commit

---
+++ modules/commands/địt.js
@@ -0,0 +1,11 @@
+﻿module.exports.config = {
+	name: "Địt",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Syn",
+	description: "Cắc Cùm Cum",
+	commandCategory: "Địt 1 ai đó",
+	cooldowns: 5
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`Tà dâm con cặc cút`, event.threadID, event.messageID);
